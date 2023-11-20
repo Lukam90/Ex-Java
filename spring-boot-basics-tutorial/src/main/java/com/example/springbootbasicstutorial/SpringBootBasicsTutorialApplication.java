0f395ec28de170eb33b524fe323cc8d607624d71
Lukam90 <lucien.hamm@gmail.com>
2023-11-20 22:09:18 +0100

    MAJ Ex Spring - MongoDB & Boot Basics (Init)

---
+++ spring-boot-basics-tutorial/src/main/java/com/example/springbootbasicstutorial/SpringBootBasicsTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbasicstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicsTutorialApplication.class, args);
+	}
+
+}
---
+++ spring-boot-basics-tutorial/src/test/java/com/example/springbootbasicstutorial/SpringBootBasicsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbasicstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
