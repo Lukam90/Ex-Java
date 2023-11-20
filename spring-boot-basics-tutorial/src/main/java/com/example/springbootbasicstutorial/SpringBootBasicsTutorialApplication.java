6d82c4c5f01fa98a194cc4e8abf5e94cf06fb2d7
Lukam90 <lucien.hamm@gmail.com>
2023-11-20 22:26:16 +0100

    MAJ Ex Spring Boot - Model

--- spring-boot-basics-tutorial/src/main/java/com/example/springbootbasicstutorial/SpringBootBasicsTutorialApplication.java
+++ spring-boot-basics-tutorial/src/main/java/com/example/springbootbasicstutorial/SpringBootBasicsTutorialApplication.java
@@ -3,11 +3,19 @@ package com.example.springbootbasicstutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class SpringBootBasicsTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBasicsTutorialApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hellow!";
+	}
 }
